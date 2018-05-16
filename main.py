0b8ca41d9e3a7f725234dd6295f01aecaecc01f8
marcus <javin.marc@gmail.com>
2018-05-16 15:43:22 +0200

    other xp in main

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ if __name__ == '__main__':
         w_v = int(sys.argv[2])
         w_ca = int(sys.argv[3])
 
-
-
-
+    name = 'Classic'
+    opt = HH_opt(init_p=params.PARAMS_RAND, init_state=params.INIT_STATE)
+    sim = HH_simul(init_p=params.DEFAULT, init_state=params.INIT_STATE, t=params.t_train, i_inj=params.i_inj_train)
+    loop_func = HodgkinHuxley.integ_comp
 
     if(xp == 'ica'):
         v_fix = True
@@ -29,13 +30,15 @@ if __name__ == '__main__':
         sim = HH_simul(init_p=params.DEFAULT, init_state=params.INIT_STATE_ica, t=params.t, i_inj=params.v_inj)
         loop_func = HodgkinHuxley.ica_from_v
 
-    else:
-        name = 'Integcomp'
-        opt = HH_opt(init_p=params.PARAMS_RAND, init_state=params.INIT_STATE)
-        sim = HH_simul(init_p=params.DEFAULT, init_state=params.INIT_STATE, t=params.t_train, i_inj=params.i_inj_train)
-        loop_func = HodgkinHuxley.integ_comp
+    elif(xp == 'notauca'):
+        name = 'Notauca'
+        loop_func = HodgkinHuxley.no_tau_ca
 
+    elif(xp == 'notau'):
+        name = 'Notau'
+        loop_func = HodgkinHuxley.no_tau
 
+        
     print(name, w_v, w_ca, loop_func)
     opt.loop_func = loop_func
     sim.loop_func = loop_func
