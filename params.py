b12eb67fa4174d0a088bcfe7c1fc2713d07510d0
RastaMarcus <javin.marc@gmail.com>
2018-05-22 12:38:33 +0200

    scripts xp

--- params.py
+++ params.py
@@ -157,8 +157,8 @@ t_train = np.array(sp.arange(0.0, 1200., DT))
 i_inj_train = 10.*((t_train>100)&(t_train<300)) + 20.*((t_train>400)&(t_train<600)) + 40.*((t_train>800)&(t_train<950))
 i_inj_train = np.array(i_inj_train, dtype=np.float32)
 
-t = np.array(sp.arange(0.0, 5000., DT))
+t = np.array(sp.arange(0.0, 2000., DT))
 i_inj = 10.*((t>100)&(t<750)) + 20.*((t>1500)&(t<2500)) + 40.*((t>3000)&(t<4000))
-v_inj = 100*(t/5000) - np.full(t.shape,50)
+v_inj = 100*(t/2000) - np.full(t.shape,50)
 i_inj = np.array(i_inj, dtype=np.float32)
 
