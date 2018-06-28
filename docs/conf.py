80cadb092094117524a92d211216ea208cbd550f
marcus <javin.marc@gmail.com>
2018-06-28 04:35:29 +0200

    mock for doc

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,20 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-print('yo')
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../opthh'))
 # sys.path.insert(1, os.path.abspath('../'))
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['tensorflow', 'numpy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
 
