2402ba11acda65e00d2676ca9a2e5094913dc910
marcus <javin.marc@gmail.com>
2018-07-30 14:57:54 +0200

    csimul tests

---
+++ tests/test_csimul.py
@@ -0,0 +1,21 @@
+"""
+.. module::
+    :synopsis: Module doing stuff...
+
+.. moduleauthor:: Marc Javin
+"""
+
+from unittest import TestCase
+from odin import circuit, csimul, neuron
+import numpy as np
+
+
+class Testcsimul(TestCase):
+
+    def test_simul(self):
+        syns = {(0,1):circuit.SYNAPSE, (1,2):circuit.SYNAPSE}
+        gaps  ={(2,1): circuit.GAP}
+        t = [0., 0.1, 0.2, 0.3]
+        i = np.ones((4,3))
+        p = [neuron.BioNeuronFix.default_params for _ in range(3)]
+        csimul.simul(t, i, p, syns, gaps)
