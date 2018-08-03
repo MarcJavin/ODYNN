9246fd77e5ae55fa396d981dd5a1bc37fff7c366
marcus <javin.marc@gmail.com>
2018-08-03 15:02:14 +0200

    agg

--- odynn/__init__.py
+++ odynn/__init__.py
@@ -2,12 +2,12 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 # Use on my server
-# import matplotlib as mpl
-# mpl.use("Agg")
-# from .utils import COLORS
-# mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=COLORS)
-# # Tune the plots appearance
-# import pylab as plt
-# SMALL_SIZE = 8
-# plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-# plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+import matplotlib as mpl
+mpl.use("Agg")
+from .utils import COLORS
+mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=COLORS)
+# Tune the plots appearance
+import pylab as plt
+SMALL_SIZE = 8
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
