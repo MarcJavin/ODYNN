b85c2399107a47a639c8adeff372bf5d614dbd3e
marcus <javin.marc@gmail.com>
2018-08-03 15:13:24 +0200

    numpy version docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['tensorflow', 'numpy']
+MOCK_MODULES = ['tensorflow']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
